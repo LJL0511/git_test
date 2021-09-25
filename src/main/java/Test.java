142ee5d0727287566c3372a7a9f4b0bbc9efae56
ljl <Lijl0511@163.com>
2021-09-25 14:54:41 +0800

    first commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @auther ljl
+ * @describe 代码描述
+ * @create 2021-09-25 14:35
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    }
+}
